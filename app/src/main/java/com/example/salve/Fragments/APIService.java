82c0f460109ea444f9cc353ce1821b7670f08df2
Sethumadhavan <zethumadhavansethu@gmail.com>
2019-10-24 20:14:45 +0530

    almost all bugs fixed

---
+++ app/src/main/java/com/example/salve/Fragments/APIService.java
@@ -0,0 +1,21 @@
+package com.example.salve.Fragments;
+
+import com.example.salve.Notifications.MyResponse;
+import com.example.salve.Notifications.Sender;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @Headers(
+            {
+                    "Content-Type:application/json",
+                    "Authorization:key=ADD HERE YOUR KEY FROM FIREBASE PROJECT"
+            }
+    )
+
+    @POST("fcm/send")
+    Call<MyResponse> sendNotification(@Body Sender body);
+}
